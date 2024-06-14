f1ff34a9e40ab0440b5c136461941564920cbc2d
YejinKimHanyang <yejinkim@hanyang.ac.kr>
2024-06-14 14:02:15 +0900

    d

--- PersonalDiagnosis_yonsanja/nu_7.java
+++ PersonalDiagnosis_yonsanja/nu_7.java
@@ -1,12 +1,17 @@
 package PersonalDiagnosis_yonsanja;
 import java.io.*;
+import java.util.StringTokenizer;
+
 public class nu_7 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //String s = br.readLine();
-        int a = Integer.parseInt(br.readline());
+        StringTokenizer st;
+        int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
 
+
+        System.out.println(a==1);
+        System.out.println(b==0);
 
 
     }
